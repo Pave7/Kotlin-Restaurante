828d878d155ebcb1b4bae6a358f5657a6139d886
andres2050h1 <andres2050h@gmail.com>
2020-03-28 17:44:16 -0500

    Acumulador de precios, comprar y reinicio de cuenta

--- app/src/main/java/com/example/familiavelasco/mexico/MainActivity.kt
+++ app/src/main/java/com/example/familiavelasco/mexico/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.ListView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    var acumulador = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,30 +17,46 @@ class MainActivity : AppCompatActivity() {
         var listview  = findViewById<ListView>(R.id.listVie)
         var list = mutableListOf<Model>()
         list.add(Model("Nachos"," Precio: $4.500",R.drawable.nachos))
-        list.add(Model("Quesadillas","Precio:$4.000",R.drawable.Quesadillas))
-        list.add(Model("Tacos","Precio:$5.000",R.drawable.Tacos))
-        list.add(Model("Burritos","Precio:$8.000",R.drawable.Burritos))
+        list.add(Model("Quesadillas","Precio:$4.000",R.drawable.quesadillas))
+        list.add(Model("Tacos","Precio:$5.000",R.drawable.tacos))
+        list.add(Model("Burritos","Precio:$8.000",R.drawable.burritos))
 
         listview.adapter=MyAdapter(this,R.layout.fila,list)
+
+        var precio0 = 4500
+        var precio1 = 4000
+        var precio2 = 5000
+        var precio3 = 8000
 
         listview.setOnItemClickListener { parent:AdapterView<*>, view:View, position:Int, id:Long ->
 
             if(position==0){
-            Toast.makeText(this@MainActivity, "description1",Toast.LENGTH_LONG).show()
+                acumulador = acumulador + precio0
+            Toast.makeText(this@MainActivity, "Valor acumulado = $"+acumulador,Toast.LENGTH_SHORT).show()
             }
             if(position==1){
-                Toast.makeText(this@MainActivity, "description2",Toast.LENGTH_LONG).show()
+                acumulador = acumulador + precio1
+                Toast.makeText(this@MainActivity, "Valor acumulado = $"+acumulador,Toast.LENGTH_SHORT).show()
             }
             if(position==2){
-                Toast.makeText(this@MainActivity, "description3",Toast.LENGTH_LONG).show()
+                acumulador = acumulador + precio2
+                Toast.makeText(this@MainActivity, "Valor acumulado = $"+acumulador,Toast.LENGTH_SHORT).show()
             }
             if(position==3){
-                Toast.makeText(this@MainActivity, "description4",Toast.LENGTH_LONG).show()
+                acumulador = acumulador + precio3
+                Toast.makeText(this@MainActivity, "Valor acumulado = $"+acumulador,Toast.LENGTH_SHORT).show()
             }
-
         }
-
     }
 
+    public fun comprar(view: View){
+        Toast.makeText(this@MainActivity, "PAGO TOTAL = $"+acumulador,Toast.LENGTH_LONG).show()
+        acumulador = 0;
+    }
+
+    public fun reiniciar(view: View){
+        Toast.makeText(this@MainActivity, "Cuenta Reiniciada",Toast.LENGTH_SHORT).show()
+        acumulador = 0;
+    }
 }
 
