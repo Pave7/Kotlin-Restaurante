98b4dbe16907bd3eaa5262df1f2f8c0e81d3c46c
unknown <paulavelasco.3096@gmail.com>
2020-04-03 21:10:01 -0500

    Comentarios

--- app/src/main/java/com/example/familiavelasco/mexico/MainActivity.kt
+++ app/src/main/java/com/example/familiavelasco/mexico/MainActivity.kt
@@ -7,56 +7,60 @@ import android.widget.AdapterView
 import android.widget.ListView
 import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
-    var acumulador = 0
+// librerias
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity() { // Clase principal que encierra la funcionalidad de las imagenes  y de la operación basica
+    var acumulador = 0  //Se inicializa el acumulador
+
+    override fun onCreate(savedInstanceState: Bundle?) {// es una clase abstracta  parecido a java
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var listview  = findViewById<ListView>(R.id.listVie)
-        var list = mutableListOf<Model>()
-        list.add(Model("Nachos"," Precio: $4.500",R.drawable.nachos))
-        list.add(Model("Quesadillas","Precio:$4.000",R.drawable.quesadillas))
-        list.add(Model("Tacos","Precio:$5.000",R.drawable.tacos))
-        list.add(Model("Burritos","Precio:$8.000",R.drawable.burritos))
+        var listview  = findViewById<ListView>(R.id.listVie) // trae la funcionalidad de la interfaz actvity_main.xml
+        var list = mutableListOf<Model>() // se implementa la interfaz de la clase "Model"  donde  el  mutableListOf es la colección de los elementos que conforman el menú
+        list.add(Model("Nachos"," Precio: $4.500",R.drawable.nachos)) //Se coloca el titulo del plato y el precio con la respectiva imagen
+        list.add(Model("Quesadillas","Precio:$4.000",R.drawable.quesadillas))//Se coloca el titulo del plato y el precio con la respectiva imagen
+        list.add(Model("Tacos","Precio:$5.000",R.drawable.tacos))//Se coloca el titulo del plato y el precio con la respectiva imagen
+        list.add(Model("Burritos","Precio:$8.000",R.drawable.burritos))//Se coloca el titulo del plato y el precio con la respectiva imagen
 
-        listview.adapter=MyAdapter(this,R.layout.fila,list)
-
+        listview.adapter=MyAdapter(this,R.layout.fila,list) //Se llama las variable de la clase MyAdapter para que funciones las imagenes y los precios dentro de la ListView
+        // precios de los platos
         var precio0 = 4500
         var precio1 = 4000
         var precio2 = 5000
         var precio3 = 8000
 
-        listview.setOnItemClickListener { parent:AdapterView<*>, view:View, position:Int, id:Long ->
+        listview.setOnItemClickListener { parent:AdapterView<*>, view:View, position:Int, id:Long -> // Esto representa el posicionamiento cuando el usuario le da clic dependiendo de las imagenes
 
-            if(position==0){
-                acumulador = acumulador + precio0
+            if(position==0){ 
+                acumulador = acumulador + precio0  //Se suma el valor acumulado más el precio del plato
             Toast.makeText(this@MainActivity, "Valor acumulado = $"+acumulador,Toast.LENGTH_SHORT).show()
             }
             if(position==1){
-                acumulador = acumulador + precio1
+                acumulador = acumulador + precio1 //Se suma el valor acumulado más el precio del plato
                 Toast.makeText(this@MainActivity, "Valor acumulado = $"+acumulador,Toast.LENGTH_SHORT).show()
             }
             if(position==2){
-                acumulador = acumulador + precio2
+                acumulador = acumulador + precio2 //Se suma el valor acumulado más el precio del plato
                 Toast.makeText(this@MainActivity, "Valor acumulado = $"+acumulador,Toast.LENGTH_SHORT).show()
             }
             if(position==3){
-                acumulador = acumulador + precio3
+                acumulador = acumulador + precio3 //Se suma el valor acumulado más el precio del plato
                 Toast.makeText(this@MainActivity, "Valor acumulado = $"+acumulador,Toast.LENGTH_SHORT).show()
             }
         }
     }
 
+    //Método para mostrar el valor total de la compra y reinicializa el acumulador
     public fun comprar(view: View){
         Toast.makeText(this@MainActivity, "PAGO TOTAL = $"+acumulador,Toast.LENGTH_LONG).show()
-        acumulador = 0;
+        acumulador = 0;     // se reinicializa el acumulador
     }
 
+    //Método para reinicializar el contador
     public fun reiniciar(view: View){
         Toast.makeText(this@MainActivity, "Cuenta Reiniciada",Toast.LENGTH_SHORT).show()
-        acumulador = 0;
+        acumulador = 0; // Se reinicializa el acumulador
     }
 }
 
--- app/src/main/java/com/example/familiavelasco/mexico/MyAdapter.kt
+++ app/src/main/java/com/example/familiavelasco/mexico/MyAdapter.kt
@@ -7,18 +7,20 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
+// librerias
 
-class MyAdapter (var mCtx: Context, var resources :Int, var items: List <Model>): ArrayAdapter<Model>(mCtx,resources,items) {
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+class MyAdapter (var mCtx: Context, var resources :Int, var items: List <Model>): ArrayAdapter<Model>(mCtx,resources,items) { // funcionalidad de listview posicionamiento de los elementos dentro de la lista
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {// es una clase abstracta  parecido a java
         val layoutInflater: LayoutInflater = LayoutInflater.from(mCtx)
         val view: View = layoutInflater.inflate(resources, null)
 
 
-        val imageView : ImageView = view.findViewById(R.id.image)
-        val titleTextView: TextView = view.findViewById(R.id.textView1)
-        val descriptionTextView: TextView = view.findViewById(R.id.textView2)
+        val imageView : ImageView = view.findViewById(R.id.image)// es la identificacion de los elementos xml
+        val titleTextView: TextView = view.findViewById(R.id.textView1)// es la identificacion de los elementos xml
+        val descriptionTextView: TextView = view.findViewById(R.id.textView2)// es la identificacion de los elementos xml
 
-        var mItem:Model=items[position]
+        var mItem:Model=items[position] // es la posicion de los elementos que conforma la lista
         imageView.setImageDrawable(mCtx.resources.getDrawable(mItem.img))
         titleTextView.text = mItem.title
         descriptionTextView.text=mItem.description
